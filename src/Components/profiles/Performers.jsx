5e066c04c8b80079cef6065b2d0658327ad8268a
Cristen <cristenrh@yahoo.com>
2021-04-03 11:33:15 -0500

    careating mobile menu, uploading placeholder images

--- src/Components/profiles/Performers.jsx
+++ src/Components/profiles/Performers.jsx
@@ -4,11 +4,11 @@ import React from 'react';
 
 const performers= ({photo, name, role, bio}) => {
     return (
-<div class="profiles-grid-container">
-  <div class="Photo"> <img src ={`${photo}`}  alt = {`${name}`}/></div>
-  <div class="Name">{name} </div>
-  <div class="Role">{role}</div>
-  <div class="Bio">{bio}</div>
+<div className="profiles-grid-container">
+  <div className="Photo"> <img src ={`${photo}`}  alt = {`${name}`}/></div>
+  <div className="Name">{name} </div>
+  <div className="Role">{role}</div>
+  <div className="Bio">{bio}</div>
 </div>
     )
 };
